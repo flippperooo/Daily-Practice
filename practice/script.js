d93a907235dfb29b6f8602241e0d603e5232983a
flippperooo <burpar10and22@proton.me>
2025-04-24 21:18:52 -0400

    JavaScript Practice
    just some super beginner practice. non usefull code.

---
+++ practice/script.js
@@ -0,0 +1,16 @@
+array = ['something', 'something else', 'something more'];
+let this_variable = array[0];
+let another_variable = array[1];
+let the_last_variable = array[2];
+console.log(this_variable);
+console.log(another_variable);
+console.log(the_last_variable);
+
+myList = ['list item', 'another list item', 'another list item'];
+
+if(myList.includes('list item')){
+  console.log('woah');
+}
+
+let true_or_false = myList.contains('an');
+console.log(true_or_false);
